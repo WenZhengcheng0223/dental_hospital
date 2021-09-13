fcb9609eda26c6605b154cdeaf8372eeb4e9d3a6
wenzhengcheng0223 <wenzhengcheng0223@163.com>
2021-09-13 20:15:33 +0800

    项目初始化

---
+++ reservation-platform-core/src/test/java/com/zmn/reservationplatformcore/ReservationPlatformCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmn.reservationplatformcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationPlatformCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
