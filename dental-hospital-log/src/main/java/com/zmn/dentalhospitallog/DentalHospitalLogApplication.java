9d094fd7b8428f00a5bba60386af2afc029485d4
wenzhengcheng0223 <wenzhengcheng0223@163.com>
2021-09-14 23:27:16 +0800

    更新 初始项目架构

---
+++ dental-hospital-log/src/main/java/com/zmn/dentalhospitallog/DentalHospitalLogApplication.java
@@ -0,0 +1,13 @@
+package com.zmn.dentalhospitallog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalHospitalLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalHospitalLogApplication.class, args);
+    }
+
+}
---
+++ dental-hospital-log/src/test/java/com/zmn/dentalhospitallog/DentalHospitalLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmn.dentalhospitallog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalHospitalLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
