b6b9d26de5744475990ffdfed50a92a72a4f7f6d
wenzhengcheng0223 <wenzhengcheng0223@163.com>
2021-09-14 17:41:34 +0800

    新增 服务器内部异常捕获处理 http状态码500

--- reservation-platform-core/src/main/java/com/zmn/exception/handler/GlobalExceptionHandler.java
+++ reservation-platform-core/src/main/java/com/zmn/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.zmn.exception.handler;
 
 import com.zmn.exception.BaseException;
 import com.zmn.exception.ErrorResponse;
+import com.zmn.exception.InternalServerErrorException;
+import com.zmn.exception.ResourcesNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -23,9 +25,45 @@ import javax.servlet.http.HttpServletRequest;
 @ResponseBody
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    /**
+     * 其他运行时异常捕获处理
+     * @param ex
+     * @param request
+     * @return
+     */
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity<?> handleException(BaseException ex, HttpServletRequest request){
+    public ResponseEntity<?> exceptionHandler(BaseException ex, HttpServletRequest request){
         ErrorResponse errorResponse = new ErrorResponse(ex, request.getRequestURI());
         return new ResponseEntity<>(errorResponse, new HttpHeaders(),ex.getError().getStatus());
+    }
+
+    /**
+     * 访问资源不存在异常捕获处理   状态码 404
+     * @param ex
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(ResourcesNotFoundException.class)
+    public ResponseEntity<ErrorResponse> resourceNotFoundExceptionHandler(
+            BaseException ex,
+            HttpServletRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(ex, request.getRequestURI());
+        return new ResponseEntity<>(errorResponse, new HttpHeaders(), ex.getError().getStatus());
+    }
+
+
+    /**
+     * 服务器内部异常捕获处理  status 500
+     * @param ex
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(InternalServerErrorException.class)
+    public ResponseEntity<ErrorResponse> internalServerErrorExceptionHandler(
+            BaseException ex,
+            HttpServletRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(ex, request.getRequestURI());
+        return new ResponseEntity<>(errorResponse, new HttpHeaders(), ex.getError().getStatus());
     }
 }
